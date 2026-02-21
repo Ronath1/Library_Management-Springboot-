ea2afb1c3526c644feb9d8f78ddd3f4b10f95dfe
Hetti Arachchige Pathumi Chamuditha <hapchamuditha@students.nsbm.ac.lk>
2026-02-21 13:24:32 +0530

    Update BookRepository

--- Springboot/src/main/java/Library_Management/Springboot/repository/BookRepository.java
+++ Springboot/src/main/java/Library_Management/Springboot/repository/BookRepository.java
@@ -1,6 +1,6 @@
-package Library_Management.Springboot.repository;
+package com.library.management.repository;
 
-import Library_Management.Springboot.entity.Book;
+import com.library.management.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

ffaa5ccfe6580ca447af88c869cdcd4d882da161
KAIA Charuka <165781808+iacharuka@users.noreply.github.com>
2026-02-23 14:41:27 +0530

    create fine
    not final because borrow and member class needed

--- Springboot/src/main/java/Library_Management/Springboot/controller/FineController.java
+++ Springboot/src/main/java/Library_Management/Springboot/controller/FineController.java
@@ -1,4 +1,37 @@
 package Library_Management.Springboot.controller;
 
+
+import Library_Management.Springboot.entity.Fine;
+import Library_Management.Springboot.repository.FineRepository;
+import Library_Management.Springboot.service.FineService;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fines")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class FineController {
+    private final FineService fineService;
+    private final FineRepository fineRepository;
+
+    @GetMapping
+    public List<Fine> getAllFines() {
+        return fineRepository.findAll();
+    }
+
+    @GetMapping("/unpaid")
+    public List<Fine> getUnpaidFines() {
+        return fineRepository.findByPaidFalse();
+    }
+
+    @PutMapping("/{id}/pay")
+    public String payFine(@PathVariable Long id) {
+        fineService.markAsPaid(id);
+        return "Fine paid successfully!";
+    }
 }

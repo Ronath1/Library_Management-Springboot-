334b75b3efcae783cd219d26f17192d9140c8e1c
Thashmila H.A,S <hasthashmila@students.nsbm.ac.lk>
2026-03-04 08:56:12 +0530

    create and update Member Repository

--- Springboot/src/main/java/Library_Management/Springboot/repository/MemberRepository.java
+++ Springboot/src/main/java/Library_Management/Springboot/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import Library_Management.Springboot.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }

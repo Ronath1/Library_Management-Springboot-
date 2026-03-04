879ca7b39eb22bce8ec8ce25d7c1eff4ff5ea339
Ronath1 <140684168+Ronath1@users.noreply.github.com>
2026-03-04 21:17:30 +0530

    Update import path for Report entity

--- Springboot/src/main/java/Library_Management/Springboot/repository/ReportRepository.java
+++ Springboot/src/main/java/Library_Management/Springboot/repository/ReportRepository.java
@@ -1,6 +1,6 @@
 package Library_Management.Springboot.repository;
 
-import com.library.management.entity.Report;
+import Library_Management.Springboot.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {

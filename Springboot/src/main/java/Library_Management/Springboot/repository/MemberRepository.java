35f2c91898e04fabde443223c9fd88da19fe1cd3
Thashmila H.A,S <hasthashmila@students.nsbm.ac.lk>
2026-03-04 07:45:56 +0530

    implement Member entity
    Created the Member entity class with fields for personal details and membership date. Established One-to-Many relationships with Borrow, Search, and Report entities using Lombok for boilerplate reduction and Jackson for JSON ignore handling.

---
+++ Springboot/src/main/java/Library_Management/Springboot/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package Library_Management.Springboot.repository;
+
+import Library_Management.Springboot.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
